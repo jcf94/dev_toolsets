87c30162d1f06417cb7863c5dd112af0a1167618
chengfan.jcf <chengfan.jcf@alibaba-inc.com>
2021-05-31 15:12:31 +0800

    [GCC] Update build scritp & Add env export

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dev Toolsets
+
+Some dev tool scripts.
--- gcc/build_gcc.sh
+++ gcc/build_gcc.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-####################################### Version Def
+####################################### Version Def & Path Def
 
 GMP_DIR="gmp-6.2.1"
 MPFR_DIR="mpfr-4.1.0"
 MPC_DIR="mpc-1.2.1"
 GCC_DIR="gcc-7.5.0"
+
+BUILT_ROOT="`pwd`/built"
+GCC_TARGET="c,c++,fortran"
+# GCC_TARGET="c,c++,fortran,go"
 
 GMP_TAR="${GMP_DIR}.tar.xz"
 GMP_URL="https://ftp.gnu.org/gnu/gmp/${GMP_TAR}"
@@ -19,11 +23,17 @@ MPC_URL="https://ftp.gnu.org/gnu/mpc/${MPC_TAR}"
 GCC_TAR="${GCC_DIR}.tar.xz"
 GCC_URL="https://ftp.gnu.org/gnu/gcc/${GCC_DIR}/${GCC_TAR}"
 
+BUILT_GMP="${BUILT_ROOT}/gmp"
+BUILT_MPFR="${BUILT_ROOT}/mpfr"
+BUILT_MPC="${BUILT_ROOT}/mpc"
+BUILT_GCC="${BUILT_ROOT}/gcc"
+BUILT_ENV="${BUILT_ROOT}/env.sh"
+
 ####################################### Prepare src files
 
 echo "Download GMP..."
 if [[ ! -f ${GMP_TAR} ]]; then
-    curl -o ${GMP_TAR} ${GMP_URL}
+    curl -L -o ${GMP_TAR} ${GMP_URL}
 fi
 echo "Uncompress GMP..."
 if [[ ! -d ${GMP_DIR} ]]; then
@@ -32,7 +42,7 @@ fi
 
 echo "Download MPFR..."
 if [[ ! -f ${MPFR_TAR} ]]; then
-    curl -o ${MPFR_TAR} ${MPFR_URL}
+    curl -L -o ${MPFR_TAR} ${MPFR_URL}
 fi
 echo "Uncompress MPFR..."
 if [[ ! -d ${MPFR_DIR} ]]; then
@@ -41,7 +51,7 @@ fi
 
 echo "Download MPC..."
 if [[ ! -f ${MPC_TAR} ]]; then
-    curl -o ${MPC_TAR} ${MPC_URL}
+    curl -L -o ${MPC_TAR} ${MPC_URL}
 fi
 echo "Uncompress MPC..."
 if [[ ! -d ${MPC_DIR} ]]; then
@@ -50,7 +60,7 @@ fi
 
 echo "Download GCC..."
 if [[ ! -f ${GCC_TAR} ]]; then
-    curl -o ${GCC_TAR} ${GCC_URL}
+    curl -L -o ${GCC_TAR} ${GCC_URL}
 fi
 echo "Uncompress GCC..."
 if [[ ! -d ${GCC_DIR} ]]; then
@@ -58,12 +68,6 @@ if [[ ! -d ${GCC_DIR} ]]; then
 fi
 
 ####################################### Build
-
-BUILT_ROOT="`pwd`/built"
-BUILT_GMP="${BUILT_ROOT}/gmp"
-BUILT_MPFR="${BUILT_ROOT}/mpfr"
-BUILT_MPC="${BUILT_ROOT}/mpc"
-BUILT_GCC="${BUILT_ROOT}/gcc"
 
 if [[ -d ${BUILT_ROOT} ]]; then
     pushd ${BUILT_ROOT}
@@ -98,6 +102,17 @@ EXTRA_LIB_PATH+="${BUILT_MPC}/lib:"
 
 # Build GCC
 pushd ${GCC_DIR}
-./configure --prefix=${BUILT_GCC} --with-gmp=${BUILT_GMP} --with-mpfr=${BUILT_MPFR} --with-mpc=${BUILT_MPC} --disable-multilib --enable-languages=c,c++,fortran,go
+./configure --prefix=${BUILT_GCC} --with-gmp=${BUILT_GMP} --with-mpfr=${BUILT_MPFR} --with-mpc=${BUILT_MPC} --disable-multilib --enable-languages=${GCC_TARGET}
 LD_LIBRARY_PATH=${EXTRA_LIB_PATH}:${LD_LIBRARY_PATH} LD_RUN_PATH=${EXTRA_LIB_PATH}:${LD_RUN_PATH} make -j && make install
 popd
+
+# Export env source file
+if [[ -f ${BUILT_ENV} ]]; then
+    rm -rf ${BUILT_ENV}
+fi
+echo "" > ${BUILT_ENV}
+echo "export C_INCLUDE_PATH=${BUILT_GCC}/include:\${C_INCLUDE_PATH}" >> ${BUILT_ENV}
+echo "export CPLUS_INCLUDE_PATH=${BUILT_GCC}/include:\${CPLUS_INCLUDE_PATH}" >> ${BUILT_ENV}
+echo "export LIBRARY_PATH=${BUILT_GCC}/lib:${BUILT_GCC}/lib64:\${LIBRARY_PATH}" >> ${BUILT_ENV}
+echo "export LD_LIBRARY_PATH=${BUILT_GCC}/lib:${BUILT_GCC}/lib64:\${LD_LIBRARY_PATH}" >> ${BUILT_ENV}
+echo "export PATH=${BUILT_GCC}/bin:\${PATH}" >> ${BUILT_ENV}
