aea2851be2403794ebd90363736981865a6472f1
jcf94 <jcf94@outlook.com>
2021-06-23 10:49:36 +0800

    [MINICONDA] Add miniconda install script

---
+++ shell/install_miniconda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MINICONDA_SH="Miniconda3-latest-Linux-x86_64.sh"
+MINICONDA_URL="https://repo.anaconda.com/miniconda/${MINICONDA_SH}"
+
+if [[ ! -f ${MINICONDA_SH} ]]; then
+    curl -L -o ${MINICONDA_SH} ${MINICONDA_URL}
+fi
+
+chmod +x ${MINICONDA_SH}
+
+./${MINICONDA_SH} -b -f -p `pwd`/miniconda
+
