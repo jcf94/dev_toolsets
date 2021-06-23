f255ba4af01146ab54622e8f888fe8ea6c25c67a
jcf94 <jcf94@outlook.com>
2021-06-23 10:46:41 +0800

    [CENTOS] Add centos basic

---
+++ system/centos_install_basic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install basic dev tools
+yum groupinstall "Development Tools" -y
+
+# Other tools
+TOOL_LIST=(
+    "epel-release"
+    "htop"
+    "tmux"
+    "vim"
+    "git"
+    "zsh"
+    "cmake"
+)
+
+for tool in ${TOOL_LIST[*]}; do
+    yum install ${tool} -y
+    done
+
