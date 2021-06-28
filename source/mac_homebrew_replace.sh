016d2db64dda4e92b0a36594a5981407c63bcc08
chengfan.jcf <chengfan.jcf@alibaba-inc.com>
2021-06-28 20:43:07 +0800

    [HOMEBREW] Update homebrew script

--- source/mac_homebrew_replace.sh
+++ source/mac_homebrew_replace.sh
@@ -1,19 +1,65 @@
 #!/bin/bash
 
-set -xe
+set -e
 
+SOURCE="tuna"
 BREW_TAPS="$(brew tap)"
-for tap in core cask{,-fonts,-drivers,-versions}; do
-    if echo "$BREW_TAPS" | grep -qE "^homebrew/${tap}\$"; then
-        # 将已有 tap 的上游设置为本镜像并设置 auto update
-        # 注：原 auto update 只针对托管在 GitHub 上的上游有效
-        git -C "$(brew --repo homebrew/${tap})" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
+BREW_BASE=""
+BREW_LIST=""
+BREW_ROOT=""
+
+####################################### Process parameters
+
+function usage() {
+    echo "./mac_homebrew_replace.sh     [-h]            Help"
+    echo "                              [-s tuna|ustc]  Set source"
+}
+
+while getopts "hs:" arg
+do
+    case "${arg}" in
+        s)
+            SOURCE="$2"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Internal error!"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ "${SOURCE}" == "tuna" ]]; then
+    BREW_BASE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+    BREW_LIST="core cask cask-fonts cask-drivers cask-versions"
+    BREW_ROOT="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew"
+elif [[ "${SOURCE}" == "ustc" ]]; then
+    BREW_BASE="https://mirrors.ustc.edu.cn/brew.git"
+    BREW_LIST="core cask cask-versions"
+    BREW_ROOT="https://mirrors.ustc.edu.cn/homebrew"
+else
+    echo "Unsupported source: ${SOURCE}"
+    exit 1
+fi
+
+####################################### Update source
+
+echo "Homebrew source update to \"${SOURCE}\""
+
+git -C "$(brew --repo)" remote set-url origin ${BREW_BASE}
+
+for tap in ${BREW_LIST}; do
+    if echo "${BREW_TAPS}" | grep -qE "^homebrew/${tap}\$"; then
+        git -C "$(brew --repo homebrew/${tap})" remote set-url origin "${BREW_ROOT}-${tap}.git"
         git -C "$(brew --repo homebrew/${tap})" config homebrew.forceautoupdate true
-    else   # 在 tap 缺失时自动安装（如不需要请删除此行和下面一行）
-        brew tap --force-auto-update homebrew/${tap} https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
+    else
+        brew tap --force-auto-update homebrew/${tap} "${BREW_ROOT}-${tap}.git"
     fi
 done
 
 brew update-reset
 
-echo "Homebrew source update success!"
+echo "Homebrew source update to \"${SOURCE}\" success!"
