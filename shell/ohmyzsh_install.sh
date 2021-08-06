3f77613326f4c5d71ebd9d47e889dd5cc7e546a1
jcf94 <jcf94@outlook.com>
2021-08-06 15:44:27 +0800

    [OHMYZSH] Update ohmyzsh script to use ys theme

--- shell/ohmyzsh_install.sh
+++ shell/ohmyzsh_install.sh
@@ -3,3 +3,7 @@
 set -xe
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Change the Theme to ys
+sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc -i
+
